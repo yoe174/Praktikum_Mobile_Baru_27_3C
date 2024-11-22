7f22b75c2e76d96e95cc852c01d91fac89852325
yoe174 <yoechannelpjr@gmail.com>
2024-11-22 10:56:50 +0700

    Praktikum 3

--- store_data/README.md
+++ store_data/README.md
@@ -5,3 +5,15 @@
 ### hasil Run
 
 ![Screenshoot yonatan](image/P1.png)
+
+## **Pratikum 2**
+
+### hasil Run
+
+![Screenshoot yonatan](image/P2.png)
+
+## **Pratikum 3**
+
+### hasil Run
+
+![Screenshoot yonatan](image/P3.png)
--- store_data/lib/main.dart
+++ store_data/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import './pizza.dart';
@@ -30,23 +31,39 @@ class _MyHomePageState extends State<MyHomePage> {
   String pizzaString = '';
   List<Pizza> myPizzas = [];
 
+  int appCounter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('JSON'),
+          title: const Text('Shared Preferences'),
         ),
         // body: Container(),
         // body: Text(pizzaString),
-        body: ListView.builder(
-            itemCount: myPizzas.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text(myPizzas[index].pizzaName),
-                  subtitle: Text(
-                    myPizzas[index].description,
-                  ));
-            }));
+        // body: ListView.builder(
+        //     itemCount: myPizzas.length,
+        //     itemBuilder: (context, index) {
+        //       return ListTile(
+        //           title: Text(myPizzas[index].pizzaName),
+        //           subtitle: Text(
+        //             myPizzas[index].description,
+        //           ));
+        //     })
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('You have opened this app $appCounter times'),
+            ElevatedButton(onPressed: () {
+              deletePreference();
+            }, child: Text('Reset counter'))
+          ],
+        )));
+  }
+
+  String convertToJSON(List<Pizza> pizzas) {
+    return jsonEncode(pizzas.map((pizza) => jsonEncode(pizza)).toList());
   }
 
   Future<List<Pizza>> readJsonFile() async {
@@ -58,15 +75,36 @@ class _MyHomePageState extends State<MyHomePage> {
       Pizza myPizza = Pizza.fromJson(pizza);
       myPizzas.add(myPizza);
     }
+    String json = convertToJSON(myPizzas);
+    print(json);
     return myPizzas;
     // setState(() {
     //   pizzaString = myString;
     // });
   }
 
+  Future readAndWritePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    appCounter = prefs.getInt('appCounter') ?? 0;
+    appCounter++;
+    await prefs.setInt('appCounter', appCounter);
+    setState(() {
+      appCounter = appCounter;
+    });
+  }
+
+  Future deletePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    setState(() {
+      appCounter = 0;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    readAndWritePreference();
     readJsonFile().then((value) {
       setState(() {
         myPizzas = value;
